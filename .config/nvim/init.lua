e8701ebead33d4576879c1d4dd407c740437c2db
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-02-21 09:31:42 -0500

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,15 @@
-qwerty
+vim.cmd("set encoding=utf-8")
+vim.cmd("set termguicolors")
+
+vim.cmd("set number")
+vim.cmd("set cursorline")
+vim.cmd("set numberwidth=4")
+vim.cmd("set tabstop=2")
+vim.cmd("set expandtab")
+vim.cmd("set smartindent")
+
+vim.cmd("set scrolloff=6")
+vim.cmd("set wrap!")
+
+vim.cmd("set listchars=space:·,eol:↩")
+vim.cmd("set list")
