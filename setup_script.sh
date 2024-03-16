2ae5e8884b3a06ea0d57bb96c0a4d4d7070279d4
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-03-16 11:27:04 -0400

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -11,17 +11,27 @@ sudo apt remove w3m -y
 sudo apt install gh -y
 sudo apt install stow -y
 
+# setup Git
+git config --global user.email "mr.coxall@mths.ca"
+git config --global user.name "Mr Coxall"
+
 # install NeoVim from source
 mkdir temp
-cd temp
-sudo apt-get install ninja-build gettext cmake unzip curl
+cd temp || exit
+sudo apt-get install ninja-build gettext cmake unzip curl -y
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
-cd ..
-cd ..
+cd .. || exit
+cd .. || exit
 sudo rm -r ./temp
+
+# install for NeoVim LSP
+sudo apt install npm -y
+sudo npm i -g typescript-language-server; sudo npm i -g typescript
 
 # install languages
 curl -fsSL https://bun.sh/install | bash
+# shellcheck source=/dev/null
+source ~/.bashrc
 sudo apt install -y default-jdk
